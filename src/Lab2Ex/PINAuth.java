ab401d44433c328e8ac2421f8e2f750ae9b9c2a2
phuocn0302 <phuocnguyen230302@gmail.com>
2025-11-17 15:19:16 +0700

    feat: add Lab2 and Lab2Ex

---
+++ src/Lab2Ex/PINAuth.java
@@ -0,0 +1,82 @@
+package Lab2Ex;
+
+import javacard.framework.*;
+
+/***
+	
+	/select 223344556700
+	
+	/select "00 ff 00 00 05 0102030405"
+	
+	/select "00 00 00 00 05 0102030401"
+	
+	/select "00 00 00 00 05 0102030405"
+
+***/
+
+public class PINAuth extends Applet
+{
+	public static final byte INS_SET_PIN = (byte) 0xff;
+	public static final byte INS_ENTER_PIN = (byte) 0x00;
+	
+	private byte[] PIN = new byte[5];
+	private short wrongTryCounter;
+
+	public static void install(byte[] bArray, short bOffset, byte bLength) 
+	{
+		new PINAuth().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
+	}
+
+	public void process(APDU apdu)
+	{
+		if (selectingApplet())
+		{
+			return;
+		}
+
+		byte[] buf = apdu.getBuffer();
+		switch (buf[ISO7816.OFFSET_INS])
+		{
+		case INS_SET_PIN:
+			setPin(apdu, buf);
+			break;
+		case INS_ENTER_PIN:
+			enterPin(apdu, buf);
+			
+			Util.setShort(buf, (short) 0, wrongTryCounter);
+			apdu.setOutgoingAndSend((short) 0, (short) 2);
+			break;
+		default:
+			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+		}
+	}
+
+
+	private void setPin(APDU apdu, byte[] buf) {
+		short len = apdu.setIncomingAndReceive();
+		
+		if (len != PIN.length) {
+			ISOException.throwIt(ISO7816.SW_DATA_INVALID);
+		}
+		
+		Util.arrayCopy(buf, (short) ISO7816.OFFSET_CDATA, PIN, (short) 0, len);
+	}
+	
+	private void enterPin(APDU apdu, byte[] buf) {
+		short len = apdu.setIncomingAndReceive();
+		
+		if (len != PIN.length) {
+			wrongTryCounter++;
+			return;
+		}
+		
+		for (short i = 0; i < len; i++) {
+			if (PIN[i] != buf[(short) (ISO7816.OFFSET_CDATA + i)]) {
+				wrongTryCounter++;
+				return;
+			}
+		}
+		
+		wrongTryCounter = 0;
+	} 
+}
---
+++ src/Lab2Ex/TempBufferInfo.java
@@ -0,0 +1,66 @@
+package Lab2Ex;
+
+import javacard.framework.*;
+
+/***
+
+	/select 223344556701
+	
+	/send "00 00 00 00 20 4354303630333330 7C 5068756F63 7C 32332F30322F32303033 7C 4861204E6F69"
+	
+	/send "00 01 00 00"
+
+***/
+
+public class TempBufferInfo extends Applet
+{
+
+	private static final byte INS_SET_INFO = (byte) 0x00;
+	private static final byte INS_GET_INFO = (byte) 0x01;
+
+	private byte[] tempBuffer;
+	
+	public static void install(byte[] bArray, short bOffset, byte bLength) 
+	{
+		new TempBufferInfo().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
+	}
+
+	public void process(APDU apdu)
+	{
+		if (selectingApplet())
+		{
+			return;
+		}
+
+		byte[] buf = apdu.getBuffer();
+		switch (buf[ISO7816.OFFSET_INS])
+		{
+		case INS_SET_INFO:
+			setInfo(apdu, buf);
+			break;
+		case INS_GET_INFO:
+			getInfo(apdu, buf);
+			break;
+		default:
+			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+		}
+	}
+	
+	private void setInfo(APDU apdu, byte[] buf) {
+		short len = apdu.setIncomingAndReceive();
+		tempBuffer = JCSystem.makeTransientByteArray(len, JCSystem.CLEAR_ON_DESELECT);
+		Util.arrayCopy(buf, ISO7816.OFFSET_CDATA, tempBuffer, (short) 0, len);
+	}
+	
+	private void getInfo(APDU apdu, byte[] buf) {
+		if (tempBuffer == null) {
+			ISOException.throwIt(ISO7816.SW_DATA_INVALID);
+		}
+		
+		Util.arrayCopy(tempBuffer, (short) 0, buf, (short) 0, (short) tempBuffer.length);
+		apdu.setOutgoingAndSend((short) 0, (short) tempBuffer.length);
+		
+		tempBuffer = null;
+	}
+	
+}
